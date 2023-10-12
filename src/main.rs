38fb7cd1bbbaa61a8ee0e36c3ac36c8d6d1e7f65
Likem <likem@pop-os.localdomain>
2023-10-12 13:31:40 +0000

    initial implem

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,13 @@ struct  Cli {
 
 fn main(){
     let args = Cli::parse();
+    let content  = std::fs::read_to_string(&args.path).expect("could not find file");
+
+    for line in content.lines(){
+        if  line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    }
 }
 
 
